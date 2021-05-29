45e022316ce6e0b0ad9b63646e074a1327ffca76
egurini <egurini@teclasystem.com>
2021-05-29 22:26:08 +0200

    Add method to know total letters, letters without spaces and words

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,41 @@ interface IState {
 function App(props: IState) {
 
   const { text, textAction } = props;
+  const [allLetters, setAllLetters] = useState('');
+  const [allLettersWithoutSpaces, setAllLettersWithoutSpaces] = useState('');
+  const [allWords, setAllWords] = useState('');
 
+  const setArrayWithAllLetters = (value : any) : void => {
+    let arrayWithAllLetters = value.split("");
+    setAllLetters(arrayWithAllLetters.length);
+    
+  }
+
+  const setArrayWithLettersButNotSpaces = (value : any) => {
+    let arrayWithAllLettersButNotSpaces = value.split("");
+    let result = arrayWithAllLettersButNotSpaces.filter((word: string) => word !== " ");
+    setAllLettersWithoutSpaces(result.length);
+  }
+
+  const setArrayWithAllWords = (value : any) => {
+    let arrayWithAllWords = value.split(" ");
+    setAllWords(arrayWithAllWords.length);
+  }
+  
   const handleChange = (e : any) :void => {
     textAction(e.target.value);
+    setArrayWithAllLetters(e.target.value);
+    setArrayWithLettersButNotSpaces(e.target.value);
+    setArrayWithAllWords(e.target.value);
   }
 
   return (
     <div className="app">
       <h1>Conta Caratteri</h1>
       <input onChange={handleChange} />
-      <p>{ text }</p>
+      <p>{ `All letters ${allLetters}` }</p>
+      <p>{ `All letters without spaces ${allLettersWithoutSpaces}` }</p>
+      <p>{ `All Words ${allWords}` }</p>
     </div>
   );
 }
